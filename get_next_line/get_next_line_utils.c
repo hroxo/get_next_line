394f18dec7696ef5695dd032e2f29aaf4934e7ff
hroxo <hugo.roxo04@gmail.com>
2025-09-15 12:54:07 +0100

    added get_next_line_utils functioning and normitted

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: hroxo <hroxo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 22:34:07 by hroxo             #+#    #+#             */
-/*   Updated: 2025/09/14 22:34:29 by hroxo            ###   ########.fr       */
+/*   Updated: 2025/09/15 12:52:24 by hroxo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,76 +16,100 @@
 
 #define BUF_SIZE 1024
 
-char	*ft_strdup(char *str, size_t cap)
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2, size_t n)
 {
 	char	*output;
 	size_t	i;
+	size_t	j;
 
+	output = malloc(n + 1);
+	j = 0;
 	i = 0;
-	output = malloc(cap * sizeof(char));
-	if (!output)
-		return (NULL);
-	while (str[i]) 
-	{
-		output[i] = str[i];
-		i++;
-	}
+	while (s1[i])
+		output[j++] = s1[i++];
+	i = 0;
+	n -= j;
+	while (i < n)
+		output[j++] = s2[i++];
+	output[j] = 0;
+	free(s1);
 	return (output);
 }
 
-char	*get_next_line(int fd)
+char	*get_more_space(char *line, size_t *cap)
 {
-	size_t	i;
-	int	f_r;
-	size_t	current_cap;
-	size_t	nb_of_bytes;
-	char	*line;
 	char	*temp;
-	char	c;
+	size_t	i;
+	size_t	current_cap;
 
+	current_cap = *cap;
+	current_cap *= 2;
+	*cap = current_cap;
 	i = 0;
-	line = malloc((BUF_SIZE + 2) * sizeof(char));
-	if (!line)
-		return (NULL);
-	f_r = read(fd, &c, 1);
-	line[i] = c;
-	nb_of_bytes = f_r; 
-	i++;
-	current_cap = BUF_SIZE;
-	while (c != '\n' && c != '\0' && f_r > 0)
+	temp = malloc(current_cap);
+	if (!temp)
 	{
-		if (nb_of_bytes >= current_cap)
-		{
-			current_cap *= 2;
-			temp = ft_strdup(line, current_cap);
-			if (!temp)
-			{
-				free(line);
-				return (NULL);
-			}
-			free(line);
-			line = temp;
-		}
-		f_r = read(fd, &c, 1);
-		line[i] = c;
-		nb_of_bytes += f_r; 
+		free(line);
+		return (NULL);
+	}
+	while (line[i])
+	{
+		temp[i] = line[i];
 		i++;
 	}
-	if (f_r < 0) 
+	temp[i] = 0;
+	free(line);
+	line = temp;
+	return (line);
+}
+
+char	*get_text(int fd, size_t nb_of_bytes, size_t current_cap)
+{
+	int		f_r;
+	char	*line;
+	char	buf[BUF_SIZE];
+
+	line = malloc((BUF_SIZE + 1) * sizeof(char));
+	if (!line)
 		return (NULL);
-	line[i] = '\0';
+	line[0] = 0;
+	f_r = read(fd, buf, BUF_SIZE);
+	while (f_r > 0)
+	{
+		if (nb_of_bytes + f_r >= current_cap)
+		{
+			line = get_more_space(line, &current_cap);
+			if (!line)
+				return (NULL);
+		}
+		line = ft_strjoin(line, buf, nb_of_bytes + f_r);
+		if (!line)
+			return (NULL);
+		nb_of_bytes += f_r;
+		f_r = read(fd, buf, BUF_SIZE);
+	}
 	return (line);
 }
 /*
 #include <fcntl.h>
-#include <stdio.h>
+#include <stdio.h> //TODO 
 
 int main(int argc, char **argv)
 {
 	(void) argc;
 	int fd = open(argv[1], O_RDONLY);
-	char *line = get_next_line(fd);
-	printf("line: %s", line);
+	char *line = get_text(fd, 0, BUF_SIZE + 1);
+	printf("%s", line);
 	free(line);
 	return 0;
 
